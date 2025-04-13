fee783b8dcc4680b391acb11d44a5d26234c63ad
MoonBasedKas <kassidy.maberry@gmail.com>
2025-04-13 00:08:11 -0600

    Syncing

--- pizzaprotocol/BlockContainer.gd
+++ pizzaprotocol/BlockContainer.gd
@@ -3,7 +3,6 @@ extends Node2D
 
 
 func _physics_process(delta):
-	
 	if Input.is_action_pressed("codeScrollUp"):
 		self.global_position += Vector2(0,10)
 	elif Input.is_action_pressed("codeScrollDown"):
